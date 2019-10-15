dae8b4e5531c2371030caa0a4b8fa7acd22b4d72
Grey <sergejyurskyj@yahoo.com>
2019-10-14 17:23:45 -0700

    commit Section 2 Diving in 6. More about Signals - Creating Custom

--- Section 2 Diving in/6. More about Signals - Creating Custom.py
+++ Section 2 Diving in/6. More about Signals - Creating Custom.py
@@ -7,15 +7,33 @@ from PySide.QtCore import *
 import sys
 
 
+class ZeroSpinBox(QSpinBox):
+
+    zeros = 0
+    atZero = Signal()
+
+    def __init__(self, parent=None):
+        super(ZeroSpinBox, self).__init__(parent)
+
+        self.valueChanged.connect(self.check_zero)
+
+    def check_zero(self, value):
+        if value == 0:
+            self.zeros += 1
+            self.constant = 5
+            self.atZero.emit()
+
+
 class Form(QDialog):
 
     def __init__(self, parent=None):
         super(Form, self).__init__(parent)
 
+
         self.dial = QDial()
         self.dial.setNotchesVisible(True)
 
-        self.spinbox = QSpinBox()
+        self.spinbox = ZeroSpinBox()
 
         layout = QVBoxLayout()
         layout.addWidget(self.dial)
@@ -25,9 +43,13 @@ class Form(QDialog):
         self.dial.valueChanged.connect(self.spinbox.setValue)
         self.spinbox.valueChanged.connect(self.dial.setValue)
 
+        self.spinbox.atZero.connect(self.printvalue)
+
+    def printvalue(self):
+        print "Caught the signal!"
+
 
 app = QApplication(sys.argv)
 form = Form()
 form.show()
 app.exec_()
-
